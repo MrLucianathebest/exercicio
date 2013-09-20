52553e14cd5cc4cfe522e3be481b0364797c86ad
vital-edu <vitaldu@gmail.com>
2013-09-20 10:45:14 -0300

    Corrigindo codigo.
    O programa estava usando os metodos diretamente da classe, sem utilizar o objeto para isso.

--- tests/ContasTest.java
+++ tests/ContasTest.java
@@ -19,27 +19,27 @@ public class ContasTest {
 	//Testar método calculoQuadrado
 	@Test
 	public void testCalculaQuadradoZero() {
-		assertEquals(0,Contas.calculaQuadrado(0),0.01);
+		assertEquals(0,umaConta.calculaQuadrado(0),0.01);
 	}
 	
 	@Test
 	public void testCalculaQuadradoPositivoPequeno() {
-		assertEquals(30.25,Contas.calculaQuadrado(5.5),0.01);
+		assertEquals(30.25,umaConta.calculaQuadrado(5.5),0.01);
 	}
 	
 	@Test
 	public void testCalculaQuadradoNegativoPequeno() {
-		assertEquals(25,Contas.calculaQuadrado(-5),0.01);
+		assertEquals(25,umaConta.calculaQuadrado(-5),0.01);
 	}
 	
 	@Test
 	public void testCalculaQuadradoPositivoGrande() {
-		assertEquals(947169,Contas.calculaQuadrado(987),0.01);
+		assertEquals(947169,umaConta.calculaQuadrado(987),0.01);
 	}
 	
 	@Test
 	public void testCalculaQuadradoNegativoGrande() {
-		assertEquals(344216.89,Contas.calculaQuadrado(-586.7),0.01);
+		assertEquals(344216.89,umaConta.calculaQuadrado(-586.7),0.01);
 	}
 	
 	
@@ -47,27 +47,27 @@ public class ContasTest {
 
 	@Test
 	public void testCalculaCuboZero() {
-		assertEquals(0,Contas.calculaCubo(0),0.01);
+		assertEquals(0,umaConta.calculaCubo(0),0.01);
 	}
 	
 	@Test
 	public void testCalculaCuboPositivoPequeno() {
-		assertEquals(125,Contas.calculaCubo(5),0.01);
+		assertEquals(125,umaConta.calculaCubo(5),0.01);
 	}
 	
 	@Test
 	public void testCalculaCuboNegativoPequeno() {
-		assertEquals(-166.375,Contas.calculaCubo(-5.5),0.01);
+		assertEquals(-166.375,umaConta.calculaCubo(-5.5),0.01);
 	}
 	
 	@Test
 	public void testCalculaCuboPositivoGrande() {
-		assertEquals(665338.617,Contas.calculaCubo(87.3),0.01);
+		assertEquals(665338.617,umaConta.calculaCubo(87.3),0.01);
 	}
 	
 	@Test
 	public void testCalculaCuboNegativoGrande() {
-		assertEquals(-941192,Contas.calculaCubo(-98),0.01);
+		assertEquals(-941192,umaConta.calculaCubo(-98),0.01);
 	}
 	
 
