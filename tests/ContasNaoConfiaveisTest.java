48f543a7c3f81c49a52a876bf759e8fc49f14b2c
vital-edu <vitaldu@gmail.com>
2013-09-20 11:05:35 -0300

    Criado a classe de teste da Classe ContasNaoConfiaveis.

--- tests/ContasNaoConfiaveisTest.java
+++ tests/ContasNaoConfiaveisTest.java
@@ -5,22 +5,73 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import ContasNaoConfiaveis;
+import contas.ContasNaoConfiaveis;
+import contas.Contas;
 
 public class ContasNaoConfiaveisTest {	
 
-	@Before
-	public void setUp() throws Exception {
-	}
+        Contas umaConta;
+        ContasNaoConfiaveis outraConta;
+        
+        @Before
+        public void setUp() throws Exception {
+                umaConta = new Contas();
+                outraConta = new ContasNaoConfiaveis();
+                
+        }
 
-	@Test
-	public void testCalculaQuadrado() {
-		fail("Not yet implemented");
-	}
+        //Testar método calculoQuadrado
+        @Test
+        public void testCalculaQuadradoZero() {
+                assertEquals(umaConta.calculaQuadrado(0),outraConta.calculaQuadrado(0),0.01);
+        }
+        
+        @Test
+        public void testCalculaQuadradoPositivoPequeno() {
+                assertEquals(umaConta.calculaQuadrado(5.5),outraConta.calculaQuadrado(5.5),0.01);
+        }
+        
+        @Test
+        public void testCalculaQuadradoNegativoPequeno() {
+                assertEquals(umaConta.calculaQuadrado(-5),outraConta.calculaQuadrado(-5),0.01);
+        }
+        
+        @Test
+        public void testCalculaQuadradoPositivoGrande() {
+                assertEquals(umaConta.calculaQuadrado(987),outraConta.calculaQuadrado(987),0.01);
+        }
+        
+        @Test
+        public void testCalculaQuadradoNegativoGrande() {
+                assertEquals(umaConta.calculaQuadrado(-586.6),outraConta.calculaQuadrado(-586.7),0.01);
+        }
+        
+        
+        //Testar método calculoCubo
 
-	@Test
-	public void testCalculaCubo() {
-		fail("Not yet implemented");
-	}
-
+        @Test
+        public void testCalculaCuboZero() {
+                assertEquals(umaConta.calculaCubo(0),outraConta.calculaCubo(0),0.01);
+        }
+        
+        @Test
+        public void testCalculaCuboPositivoPequeno() {
+                assertEquals(umaConta.calculaCubo(5),outraConta.calculaCubo(5),0.01);
+        }
+        
+        @Test
+        public void testCalculaCuboNegativoPequeno() {
+                assertEquals(umaConta.calculaCubo(-5.5),outraConta.calculaCubo(-5.5),0.01);
+        }
+        
+        @Test
+        public void testCalculaCuboPositivoGrande() {
+                assertEquals(umaConta.calculaCubo(87.3),outraConta.calculaCubo(87.3),0.01);
+        }
+        
+        @Test
+        public void testCalculaCuboNegativoGrande() {
+                assertEquals(umaConta.calculaCubo(-98),outraConta.calculaCubo(-98),0.01);
+        }
+        
 }
